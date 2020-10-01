db2ea3fc6b6767b3c7c96bada867ea41329460b5
DJLee68 <ted4562000@naver.com>
2020-10-01 14:31:28 +0900

    multiplication table

---
+++ multi-table.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+echo "printout user name: " $USER
+first_val=$1
+second_val=$2
+if [ $first_val -lt 1 ] || [ $second_val -lt 1 ]
+then
+    echo "Value must be greater than or equal to 1"
+    exit 0
+fi 
+
+for i in $(seq 1 $first_val)
+do       
+    for j in $(seq 1 $second_val)
+    do
+	echo -n $i "*" $j "=" `expr $i \* $j` " "
+    done
+    echo 
+done
+exit 0
